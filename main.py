c7f3a227935a61d670bd8a84059bb193cb8423bd
bernardopoliv <bernardo.perina@petco.com>
2021-05-13 19:28:13 -0300

    delete all files after reading

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import argparse
+import asyncio
+import glob
+import os
+import random
 from concurrent.futures import ProcessPoolExecutor
 from datetime import datetime, timedelta
 from time import sleep
@@ -6,8 +11,21 @@ import requests
 from bs4 import BeautifulSoup
 from pdfminer.high_level import extract_text
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 import settings
+
+parser = argparse.ArgumentParser(description='Look for your name in Ceará vaccination lists.')
+parser.add_argument('-k', '--keep', action='store_true', help='keep downloaded files in files directory.')
+
+
+async def perform_request(url):
+    print(url)
+    loop = asyncio.get_event_loop()
+    try:
+        return loop.run_in_executor(None, requests.get, url)
+    except:
+        return
 
 
 def download(initial_date: str, days_ahead: int = 1):
@@ -15,32 +33,33 @@ def download(initial_date: str, days_ahead: int = 1):
         # Default: current day
         initial_date = datetime.today().strftime('%d/%m/%Y')
 
-    browser = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless")
+    browser = webdriver.Firefox(firefox_options=options)
     browser.get(settings.VAC_PUBLIC_LIST_URL)
-    sleep(4)
-
+    sleep(3)
     urls = find_urls(browser, initial_date, days_ahead)
     print(f'Found {len(urls)} lists. Downloading...')
 
-    file_names = _download(urls)
+    file_names = asyncio.run(_download(urls))
     print('File(s) downloaded successfully!\n')
     return file_names
 
 
-def _download(urls):
+async def _download(urls):
     filenames = []
+    futures = await asyncio.gather(*[
+        perform_request(url['url']) for url in urls
+    ])
 
-    for i, url in enumerate(urls, start=1):
-        try:
-            response = requests.get(url['url'])
-        except Exception:
-            continue
-
-        filename = f'{settings.ROOT_DIR}file_{url["date"]}__{i}.pdf'
+    for coro in asyncio.as_completed(futures):
+        response = await coro
+        filename = f'{settings.ROOT_DIR}/files/file__{random.randint(1, 300)}.pdf'
         filenames.append(filename)
 
         with open(filename, 'wb') as f:
             f.write(response.content)
+
     return filenames
 
 
@@ -54,8 +73,8 @@ def find_urls(browser, initial_date, days_ahead):
             for x in range(days_ahead)
         ]
         for day in date_range:
-            if day.strftime('%d/%m/%Y') in i.text:
-                url = i['href']
+            url = i['href']
+            if day.strftime('%d/%m/%Y') in i.text and 'http' in url:
                 urls.append({"url": url, "date": day.strftime('%d_%m_%Y')})
 
     return urls
@@ -67,10 +86,8 @@ def _read(filename):
     except Exception:
         return
 
-    found = []
-    for name in settings.NAME_LOOKUPS:
-        if name in results:
-            found.append(name)
+    found = [name for name in settings.NAME_LOOKUPS if name in results]
+
     print(
         f'{filename.split("/")[-1]}: '
         f'{found if found else "No results in this file."}'
@@ -82,6 +99,20 @@ def read(filenames):
         pool.map(_read, filenames)
 
 
+def delete_files(file_directory):
+    files = glob.glob('{}/*'.format(file_directory), recursive=True)
+    for f in files:
+        try:
+            os.remove(f)
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
+
+
 if __name__ == '__main__':
+    args = parser.parse_args()
+
     filenames = download(settings.INITIAL_DATE, settings.DAYS_AHEAD)
     read(filenames=filenames)
+
+    if not args.keep:
+        delete_files(settings.FILES_DIR)
