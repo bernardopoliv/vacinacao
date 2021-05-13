380655806f9b6e16fc3184dd6f77375684571d68
bernardopoliv <bernardo.perina@petco.com>
2021-05-13 19:25:55 -0300

    improvements and more logging

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def download(date=None):
 
     browser = webdriver.Firefox()
     browser.get(VAC_PUBLIC_LIST_URL)
-    # sleep(4)
+    sleep(4)
     html = browser.page_source
     soup = BeautifulSoup(html, 'lxml')
 
@@ -25,6 +25,8 @@ def download(date=None):
         if date in i.text:
             url = i['href']
             urls.append(url)
+
+    print(f'Found {len(urls)} lists for {date}. Downloading...')
 
     files = []
     for i, url in enumerate(urls, start=1):
@@ -35,17 +37,23 @@ def download(date=None):
         with open(filename, 'wb') as f:
             f.write(response.content)
 
+    print('File(s) downloaded successfully!\n')
     return files
 
 
 def read(filenames: list):
-    for f in filenames:
+    print(f'Reading {len(filenames)} files.')
+    for i, f in enumerate(filenames, 1):
+        print(f'Parsing {f.split("/")[-1]}')
         text = extract_text(f)
         for name in NAME_LOOKUPS:
             if name in text:
-                print(f'{f}: {name}')
+                print(f'{f}: {name}\n')
+                break
+        else:
+            print('No results in this file.\n')
 
 
 if __name__ == '__main__':
-    filenames = download('05/05/2021')
+    filenames = download('29/04/2021')
     read(filenames)
