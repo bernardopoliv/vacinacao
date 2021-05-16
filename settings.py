b0f1ead8980e71c90bc28a8e4c5e4bb5905eda81
= <joaopbernhardt@gmail.com>
2021-05-16 15:18:41 -0300

    Add geckodriver-autoinstaller for easier setup

--- settings.py
+++ settings.py
@@ -1,13 +1,18 @@
 import os
 
-NAME_LOOKUPS = []
+NAME_LOOKUPS = ["FULANO DA SILVA"]
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 VAC_PUBLIC_LIST_URL = 'https://coronavirus.fortaleza.ce.gov.br/lista-vacinacao-d1.html'
 
-INITIAL_DATE = '05/05/2021'
+INITIAL_DATE = '16/05/2021'
 FILES_DIR = f'{ROOT_DIR}/files'
 DAYS_AHEAD = 10
 
 
 # Cloud settings
 S3_FILES_BUCKET = 'vacinacao-covid-ceara'
+
+
+# Downloads geckodriver and puts it in $PATH
+import geckodriver_autoinstaller
+geckodriver_autoinstaller.install()
