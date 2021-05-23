811559dcd94add7b82387fa546db0ff369312169
= <joaopbernhardt@gmail.com>
2021-05-23 19:19:09 -0300

    Index is now gzipped

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 build-and-push:
 	docker-compose build
-	docker tag vacinacao-img:latest 244978745220.dkr.ecr.us-east-1.amazonaws.com/vacinacao-app:0.6.0
-	docker push 244978745220.dkr.ecr.us-east-1.amazonaws.com/vacinacao-app:0.6.0
+	docker tag vacinacao-img:latest 244978745220.dkr.ecr.us-east-1.amazonaws.com/vacinacao-app:0.7.0
+	docker push 244978745220.dkr.ecr.us-east-1.amazonaws.com/vacinacao-app:0.7.0
 
 deploy:
 	sam deploy --no-confirm-changeset
--- src/vacinacao/indexer.py
+++ src/vacinacao/indexer.py
@@ -1,7 +1,8 @@
-from datetime import date
 import json
+import gzip
+from datetime import date
 
-from vacinacao import s3
+from vacinacao import s3, settings
 from vacinacao.log_utils import setup_logging
 
 
@@ -24,23 +25,36 @@ def compile_index():
     logger.info("Dumped index into JSON string.")
 
     index_filename = _get_todays_index()
-    s3.upload(index_filename, bytes(index_json, encoding="utf8"))
+    s3.upload(index_filename, gzip.compress(bytes(index_json, encoding="utf8")))
 
     logger.info(f"Successfully wrote index file '{index_filename}'.")
 
 
 def pull_index():
     index_filename = _get_todays_index()
+    logger.info(f"Looking for index '{index_filename}'.")
     if s3.file_exists(index_filename):
-        index_json = s3.pull(index_filename).decode()
-        return json.loads(index_json)
+        index_json = gzip.decompress(s3.pull(index_filename)).decode()
+        index = json.loads(index_json)
+        logger.info(f"Index contains '{len(index)}' files.")
+        return index
     else:
         raise IndexUnavailable()
 
 
 def _get_todays_index():
-    return f"index_{date.today().isoformat()}.json"
+    return f"index_{date.today().isoformat()}.json.gzip"
 
 
 if __name__ == "__main__":
+    from vacinacao.main import download, generate_results
+
+    logger.info('Starting reindex...')
+    existing_files = s3.get_existing_files(settings.S3_FILES_BUCKET)
+    download(existing_files)
+
+    logger.info("Generating results...")
+    generate_results(existing_files)
+
+    logger.info("Compiling index...")
     compile_index()
