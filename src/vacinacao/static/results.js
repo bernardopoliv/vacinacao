70554f1de5f51676abf1c8a43a07f7035a87fece
bernardopoliv <bernardo.perina@petco.com>
2021-05-26 22:07:34 -0300

    remove previous results when user makes another search

--- src/vacinacao/static/results.js
+++ src/vacinacao/static/results.js
@@ -16,16 +16,26 @@ function createRows(response) {
   })
 }
 
+function removeResults(elementId) {
+  const results = document.getElementById(elementId);
+  while (results.firstChild) {
+    results.removeChild(results.lastChild);
+  }
+}
+
 document.getElementById('search').addEventListener('click', function () {
+  const noInputWarning = document.getElementById('no-input-warning')
   const nameInput = document.getElementById('name')
   const name = nameInput.value
   if (!name.trim()) {
-    document.getElementById('no-input-warning').hidden = false
+    noInputWarning.style.display = ''  // Visible
   } else {
+    noInputWarning.style.display = 'none'  // Invisible
+
     postData('/search', {name: nameInput.value})
       .then(data => {
-        document.getElementById('search-container').style.display = 'none'
         document.getElementById('results-table').style.display = ''
+        removeResults('results')  // Remove to display a new one
         createRows(data);
       });
   }
